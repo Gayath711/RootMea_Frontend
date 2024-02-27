90688d1853c33a4b647c08a0541c29d0ce25f864
MonishDataterrain <monish@dataterrain>
2024-02-27 15:01:25 +0530

    signup

--- my-react-app/src/components/Signup.js
+++ my-react-app/src/components/Signup.js
@@ -26,7 +26,7 @@ const SignupPage = () => {
 
   const handlesignup = async () => {
     try {
-      const response = await fetch('http://127.0.0.1:8000/api/signup/', {
+      const response = await fetch('http://192.168.3.24:8000/api/signup/', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
