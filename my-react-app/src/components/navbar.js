0fa9a1a7f8a723cbe6043a9f1fde64757cc7cc72
mathisdataterrain <mathis@dataterrain.com>
2024-03-03 09:33:12 +0530

    bug fixing

--- my-react-app/src/components/navbar.js
+++ my-react-app/src/components/navbar.js
@@ -66,11 +66,13 @@ const Navbar = ({ onLogout, isMinimized, toggleSidebar }) => {
   {profileType === 'admin' && (
     <div>
       <button className="bg-green-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-        <a href="http://localhost:8000/admin/">Admin</a>
+        <a href="http://192.168.3.24:8000/admin/">Admin</a>
       </button>
     </div>
   )}
 </div>
+
+
 
 
 
