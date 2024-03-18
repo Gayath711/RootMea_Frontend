4cb595b9f83201b4bfa7f2cd43b61c83c1695bc6
Rushil-2311 <rushilpatellinux@gmail.com>
2024-03-18 13:17:34 +0530

    missing changes

--- src/store/index.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import rootReducer from "./reducers";
-import { setStoreInstance } from "../services/api";
-
-const store = configureStore({ reducer: rootReducer });
-
-setStoreInstance(store);
-
-export default store;
