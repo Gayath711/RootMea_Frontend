f4dd73c6f7c53a081643d01b85a52871f838d658
Raj Kumar D <rajkumar.d@instellars.com>
2024-04-03 15:09:58 +0530

    Fix: Login Validation

--- my-react-app/src/components/Login/Logins.js
+++ my-react-app/src/components/Login/Logins.js
@@ -5,11 +5,10 @@ import { loginAsync } from "../../store/slices/authSlice";
 import { useForm } from "react-hook-form";
 
 import RootsLogo from "../../image/root.png";
-import apiURL from '../.././apiConfig';
+import apiURL from "../.././apiConfig";
 import PasswordReset from "./PasswordReset";
-import AlertSuccess from '../common/AlertSuccess';
+import AlertSuccess from "../common/AlertSuccess";
 import AlertError from "../common/AlertError";
-
 
 const LoginForm = () => {
   const [isFlipped, setIsFlipped] = useState(false);
@@ -21,11 +20,11 @@ const LoginForm = () => {
 
   const closeSuccessAlert = () => {
     setSuccessShowAlert(false);
-  }
+  };
 
   const closeErrorAlert = () => {
     setErrorShowAlert(false);
-  }
+  };
 
   const {
     register,
@@ -54,15 +53,15 @@ const LoginForm = () => {
   // };
 
   const handleLogin = (loginData) => {
-    console.log("Login data - 1", loginData.userEmail)
-    console.log("data length", Object.keys(errorsLogin).length)
-    console.log("errorsLogin", errorsLogin)
+    console.log("Login data - 1", loginData.userEmail);
+    console.log("data length", Object.keys(errorsLogin).length);
+    console.log("errorsLogin", errorsLogin);
     if (Object.keys(errorsLogin).length < 1) {
       const user = {
         email: loginData.userEmail,
         password: loginData.password,
       };
-      console.log("Login data - 2", user)
+      console.log("Login data - 2", user);
 
       // axios.post(`${apiURL}/token/`, user)
       //   .then(response => {
@@ -87,29 +86,30 @@ const LoginForm = () => {
       } catch (error) {
         console.error("Login error:", error);
       }
-
     }
   };
 
   const handlePasswordReset = (resetData) => {
     console.log(resetData);
 
-    axios.post(`${apiURL}/api/password/reset/`, resetData)
-      .then(response => {
-        console.log(response.data)
+    axios
+      .post(`${apiURL}/api/password/reset/`, resetData)
+      .then((response) => {
+        console.log(response.data);
         setSuccessMsg("Password reset email has been sent");
         setErrorShowAlert(false);
         setSuccessShowAlert(true);
       })
-      .catch(error => {
-        console.error('Error fetching Client Diagnosis Data:', error.response.data);
+      .catch((error) => {
+        console.error(
+          "Error fetching Client Diagnosis Data:",
+          error.response.data
+        );
         setErrorMsg(error?.response?.data?.error);
         setSuccessShowAlert(false);
         setErrorShowAlert(true);
       });
-
-  }
-
+  };
 
   return (
     <div
@@ -118,14 +118,34 @@ const LoginForm = () => {
     >
       <div className={`flex items-center justify-center h-screen`}>
         {isFlipped ? (
-          <div className={`w-96 h-fit flex flex-col bg-white rounded shadow`} style={{
-            transformStyle: 'preserve-3d', transition: 'transform 0.5s', transform: isFlipped ? 'rotateY(180deg)' : 'rotateY(0deg)'
-          }}>
-            {showSuccessAlert && (<div style={{ transform: 'rotateY(180deg)' }}><AlertSuccess message={successMsg} handleClose={closeSuccessAlert} /> </div>)}
-            {showErrorAlert && (<div style={{ transform: 'rotateY(180deg)' }}><AlertError message={errorMsg} handleClose={closeErrorAlert} /> </div>)}
+          <div
+            className={`w-96 h-fit flex flex-col bg-white rounded shadow`}
+            style={{
+              transformStyle: "preserve-3d",
+              transition: "transform 0.5s",
+              transform: isFlipped ? "rotateY(180deg)" : "rotateY(0deg)",
+            }}
+          >
+            {showSuccessAlert && (
+              <div style={{ transform: "rotateY(180deg)" }}>
+                <AlertSuccess
+                  message={successMsg}
+                  handleClose={closeSuccessAlert}
+                />{" "}
+              </div>
+            )}
+            {showErrorAlert && (
+              <div style={{ transform: "rotateY(180deg)" }}>
+                <AlertError message={errorMsg} handleClose={closeErrorAlert} />{" "}
+              </div>
+            )}
             <form onSubmit={handleSubmit(handlePasswordReset)}>
-              <div style={{ transform: 'rotateY(180deg)' }}>
-                <div className={`flex flex-col items-center space-y-0 pb-5 ${showSuccessAlert || showErrorAlert ? "pt-0" : "pt-5"}`}  >
+              <div style={{ transform: "rotateY(180deg)" }}>
+                <div
+                  className={`flex flex-col items-center space-y-0 pb-5 ${
+                    showSuccessAlert || showErrorAlert ? "pt-0" : "pt-5"
+                  }`}
+                >
                   <img className="w-24 h-20" src={RootsLogo} />
                   <div className="font-medium text-2xl pt-3">
                     Reset Password
@@ -141,16 +161,22 @@ const LoginForm = () => {
                       required: "Email Address is required",
                       pattern: {
                         value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
-                        message: "Invalid email address"
-                      }
+                        message: "Invalid email address",
+                      },
                     })}
                   />
-                  {errors.email && <p role="alert" className="text-red-500 text-xs">{errors.email.message}</p>}
+                  {errors.email && (
+                    <p role="alert" className="text-red-500 text-xs">
+                      {errors.email.message}
+                    </p>
+                  )}
                   <div className="flex w-full justify-evenly pb-5 pt-12">
                     <div className="p-3">
                       <button
                         className="w-28 h-8 border-1 border-[#0F7235] text-md rounded-sm"
-                        onClick={() => { setIsFlipped(!isFlipped) }}
+                        onClick={() => {
+                          setIsFlipped(!isFlipped);
+                        }}
                       >
                         Cancel
                       </button>
@@ -168,48 +194,45 @@ const LoginForm = () => {
               </div>
             </form>
           </div>
-        ) :
-          (<div className={`w-96 h-fit flex flex-col bg-white rounded shadow pt-5`} style={{
-            transformStyle: 'preserve-3d', transition: 'transform 0.5s', transform: isFlipped ? 'rotateY(180deg)' : 'rotateY(0deg)'
-          }}>
+        ) : (
+          <div
+            className={`w-96 h-fit flex flex-col bg-white rounded shadow pt-5`}
+            style={{
+              transformStyle: "preserve-3d",
+              transition: "transform 0.5s",
+              transform: isFlipped ? "rotateY(180deg)" : "rotateY(0deg)",
+            }}
+          >
             <form onSubmit={handleSubmitLogin(handleLogin)}>
               <div className="flex flex-col items-center space-y-0 pb-5 pt-0">
                 <img className="w-24 h-20" src={RootsLogo} />
-                <div className="font-medium text-2xl pt-3">
-                  Welcome
-                </div>
+                <div className="font-medium text-2xl pt-3">Welcome</div>
               </div>
               <div className="">
                 <div className="flex flex-col items-start px-4">
-                  <div className=" text-gray-800 text-opacity-50 text-xs font-normal">
+                  <div className="text-gray-800 text-opacity-50 text-xs font-normal">
                     Enter Roots Email Address
                   </div>
                   <input
-                    class="border-b border-gray-800  focus:outline-none px-2 py-1 w-full mb-2"
-                    // type="text"
-                    // placeholder=""
-                    // id="userEmail"
-                    // value={username}
-                    // onChange={handleUsernameChange}
+                    className="border-b border-gray-800 focus:outline-none px-2 py-1 w-full mb-2"
                     {...registerLogin("userEmail", {
                       required: "Email Address is required",
                       pattern: {
                         value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
-                        message: "Invalid email address"
-                      }
+                        message: "Invalid email address",
+                      },
                     })}
                   />
-                  {errorsLogin.userEmail && <p role="alert" className="text-red-500 text-xs pb-3">{errorsLogin.userEmail.message}</p>}
-                  <div className=" text-gray-800 text-opacity-50 text-xs font-normal">
+                  {errorsLogin.userEmail && (
+                    <p role="alert" className="text-red-500 text-xs pb-3">
+                      {errorsLogin.userEmail.message}
+                    </p>
+                  )}
+                  <div className="text-gray-800 text-opacity-50 text-xs font-normal">
                     Enter Roots Password
                   </div>
                   <input
-                    class="border-b border-gray-800  focus:outline-none px-2 py-1 w-full mb-2"
-                    // type="password"
-                    // id="password"
-                    // placeholder=""
-                    // value={password}
-                    // onChange={handlePasswordChange}
+                    className="border-b border-gray-800 focus:outline-none px-2 py-1 w-full mb-2"
                     {...registerLogin("password", {
                       required: "Password is required",
                     })}
@@ -227,32 +250,33 @@ const LoginForm = () => {
                     <div className="w-8 h-4 left-0 top-0 absolute bg-zinc-300 rounded-xl" />
                     <div className="w-4 h-4 left-0 top-0 absolute rounded-full border border-black" />
                   </div>
-                  <div className=" text-black-500 text-xs font-normal">
+                  <div className="text-black-500 text-xs font-normal">
                     Stay Signed In
                   </div>
                 </div>
-                <button className=" text-black-500 text-xs font-normal"
+                <button
+                  className="text-black-500 text-xs font-normal"
                   onClick={() => {
                     setIsFlipped(!isFlipped);
                     setErrorShowAlert(false);
                     setSuccessShowAlert(false);
                     reset();
-                  }}>
+                  }}
+                >
                   Reset Your Password
                 </button>
               </div>
               <div className="flex flex-col items-center pb-5">
                 <button
+                  type="submit"
                   className="w-36 h-10 border-1 bg-[#0F7235] text-white text-md rounded-sm"
-                  onClick={handleLogin}
                 >
                   SIGN IN
                 </button>
-                {/* <p className=" text-black text-xs font-normal">OR</p>
-          <p className=" text-black text-xs font-normal">If you have loss your official email or password, Please contact your help desk support team.</p> */}
               </div>
             </form>
-          </div>)}
+          </div>
+        )}
       </div>
       {/* {
         showModal && (
@@ -262,7 +286,7 @@ const LoginForm = () => {
       <div className="absolute bottom-0 right-0">
         <img src="./login/flower-pot.png" className="w-44 h-72"></img>
       </div>
-    </div >
+    </div>
   );
 };
 
