120b608d7281540994d3c2242b2c496ae5ec2d50
Raj Kumar D <rajkumar.d@instellars.com>
2024-04-24 19:24:36 +0530

    Fix: Dashboard Icons Size

--- my-react-app/src/components/SideBar/SideBar.jsx
+++ my-react-app/src/components/SideBar/SideBar.jsx
@@ -1,76 +1,111 @@
-import LeftBarIcon1 from "../images/leftBarIcon1.svg";
-import LeftBarIcon2 from "../images/leftBarIcon2.svg";
-import LeftBarIcon3 from "../images/leftBarIcon3.svg";
-import LeftBarIcon4 from "../images/client-profile-active.svg";
-import LeftBarIcon5 from "../images/calender-active.svg";
-import LeftBarIcon6 from "../images/form-active.svg";
-import LeftBarIcon7 from "../images/directory-active.svg";
-import { Link } from "react-router-dom";
+import React from "react";
+import { Link, useLocation } from "react-router-dom";
 
-import NewClientIconActive from "../images/side_bar/client-profile-active.svg";
+import DashboardActive from "../images/side_bar/dashboard-active.png";
+import DashboardInActive from "../images/side_bar/dashboard-inactive.png";
 
-const SideBar = () => {
+import CalendarActive from "../images/side_bar/calendar-active.png";
+import CalendarInActive from "../images/side_bar/calendar-inactive.png";
+
+import FormBuilderActive from "../images/side_bar/formbuilder-active.png";
+import FormBuilderInActive from "../images/side_bar/formbuilder-inactive.png";
+
+import NewClientActive from "../images/side_bar/newclient-active.png";
+import NewClientInActive from "../images/side_bar/newclient-inactive.png";
+
+import DirectoryActive from "../images/side_bar/directory-active.png";
+import DirectoryInActive from "../images/side_bar/directory-inactive.png";
+
+import YetAnotherLinkActive from "../images/side_bar/yetanotherlink-active.png";
+import YetAnotherLinkInActive from "../images/side_bar/yetanotherlink-inactive.png";
+
+let sidebarLinks = [
+  {
+    id: "dashboard-page",
+    to: "/",
+    title: "Home",
+    activeImageSrc: DashboardActive,
+    inactiveImageSrc: DashboardInActive,
+    isActive: false,
+  },
+  {
+    id: "calendar-page",
+    to: "/calendar",
+    title: "Calendar",
+    activeImageSrc: CalendarActive,
+    inactiveImageSrc: CalendarInActive,
+    isActive: false,
+  },
+  {
+    id: "form-builder-page",
+    to: "/form_builder",
+    title: "Form Builder",
+    activeImageSrc: FormBuilderActive,
+    inactiveImageSrc: FormBuilderInActive,
+    isActive: false,
+  },
+  {
+    id: "directory-page",
+    to: "/directory",
+    title: "Directory",
+    activeImageSrc: DirectoryActive,
+    inactiveImageSrc: DirectoryInActive,
+    isActive: false,
+  },
+  {
+    id: "client-profile-new",
+    to: "/clientprofilenew",
+    title: "New Client",
+    activeImageSrc: NewClientActive,
+    inactiveImageSrc: NewClientInActive,
+    isActive: false,
+  },
+  {
+    title: "Yet Another Link",
+    to: "/#",
+    activeImageSrc: YetAnotherLinkActive,
+    inactiveImageSrc: YetAnotherLinkInActive,
+    isActive: false,
+  },
+  // Add more links if needed
+];
+const Sidebar = () => {
+  const location = useLocation();
+
   return (
     <div className="flex flex-col justify-center items-center space-y-12 pt-8 pb-24 bg-white shadow-2xl rounded-br-[2rem] sticky left-0 top-[100px]">
-      <Link to="/">
-        <button className="p-1 bg-[#D4EDEC]" id="dashboard-page" title="Home">
-          <img src={LeftBarIcon1} className="size-4" alt="icon1" />
-        </button>
-      </Link>
-
-      <Link to="/calendar">
-        <button
-          className="p-1 bg-[#D4EDEC]"
-          id="calendar-page"
-          title="Calendar"
-        >
-          <img src={LeftBarIcon5} className="size-4" alt="icon5" />
-        </button>
-      </Link>
-
-      <Link to="/form_builder">
-        <button
-          className="p-1 bg-[#EAECEB]"
-          id="form-builder-page"
-          title="Form Builder"
-        >
-          <img src={LeftBarIcon6} className="size-4" alt="icon6" />
-        </button>
-      </Link>
-
-      <Link to="/directory">
-        <button
-          className="p-1 bg-[#EAECEB]"
-          id="directory-page"
-          title="Directory"
-        >
-          <img src={LeftBarIcon7} className="size-4" alt="icon7" />
-        </button>
-      </Link>
-
-      <Link to="/clientprofilenew">
-        <button
-          className="p-1 bg-[#EAECEB]"
-          id="client-profile-new"
-          title="New Client"
-        >
-          <img src={NewClientIconActive} className="size-5" alt="icon4" />
-        </button>
-      </Link>
-
-      <button className="p-1 bg-[#EAECEB]" title="Another Link">
-        <img src={LeftBarIcon2} className="size-4" alt="icon2" />
-      </button>
-
-      <button className="p-1 bg-[#EAECEB] " title="Yet Another Link">
-        <img src={LeftBarIcon3} className="size-4" alt="icon6" />
-      </button>
-
-      {/* <button className="p-1 bg-[#EAECEB]">
-        <img src={LeftBarIcon8} className="size-4" alt="icon7" />
-      </button> */}
+      {sidebarLinks.map((link, index) => {
+        const isActive = location.pathname === link.to;
+        const bgColor = isActive ? "#D4EDEC" : "#EAECEB";
+        return (
+          <Link
+            key={index}
+            to={link.to}
+            // onMouseEnter={(e) => {
+            //   if (!link.isActive) {
+            //     e.target.style.backgroundColor = "#D4EDEC";
+            //     e.target.src = link.activeImageSrc;
+            //   }
+            // }}
+            // onMouseLeave={(e) => {
+            //   if (!link.isActive) {
+            //     e.target.style.backgroundColor = "#EAECEB";
+            //     e.target.src = link.inactiveImageSrc;
+            //   }
+            // }}
+          >
+            <img
+              className={`p-1 bg-[${bgColor}] size-6`}
+              id={link.id}
+              src={isActive ? link.activeImageSrc : link.inactiveImageSrc}
+              alt={`icon${index + 1}`}
+              title={link.title}
+            />
+          </Link>
+        );
+      })}
     </div>
   );
 };
 
-export default SideBar;
+export default Sidebar;
