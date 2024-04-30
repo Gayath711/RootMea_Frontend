bc8a1c7cb155e1a2ae9a6c9522af6caf046ecef2
Raj Kumar D <rajkumar.d@instellars.com>
2024-04-30 09:02:19 +0530

    Update: Side bar with Dropdown

--- my-react-app/src/components/SideBar/SideBar.jsx
+++ my-react-app/src/components/SideBar/SideBar.jsx
@@ -7,6 +7,8 @@ import {
   selectIsSidebarExpanded,
 } from "../../store/slices/utilsSlice";
 
+import MenuDropDownArrow from "../images/side_bar/menu-down-arrow.svg";
+
 import DashboardActive from "../images/side_bar/dashboard-active.png";
 import DashboardInActive from "../images/side_bar/dashboard-inactive.png";
 
@@ -34,6 +36,7 @@ let sidebarLinks = [
     inactiveImageSrc: DashboardInActive,
     isActive: false,
   },
+
   {
     id: "calendar-page",
     to: "/calendar",
@@ -57,6 +60,18 @@ let sidebarLinks = [
     activeImageSrc: DirectoryActive,
     inactiveImageSrc: DirectoryInActive,
     isActive: false,
+    children: [
+      {
+        id: "staff-directory",
+        to: "/staff-directory",
+        title: "Staff Directory",
+      },
+      {
+        id: "program-directory",
+        to: "/program-directory",
+        title: "Program  Directory",
+      },
+    ],
   },
   {
     id: "client-profile-new",
@@ -80,6 +95,8 @@ const Sidebar = () => {
 
   // const [showTitle, setShowTitle] = useState(false);
 
+  const [showNested, setShowNested] = useState([]);
+
   const isSidebarExpanded = useSelector(selectIsSidebarExpanded);
   const dispatch = useDispatch();
 
@@ -98,71 +115,85 @@ const Sidebar = () => {
         // setShowTitle(false);
       }}
       style={{
-        width: `${isSidebarExpanded ? "140px" : "50px"}`,
+        width: `${isSidebarExpanded ? "165px" : "50px"}`,
         transition: "all ease 0.3s",
       }}
-      className={`flex flex-col ${
-        isSidebarExpanded ? "justify-start px-3" : "items-center"
-      } space-y-12 pt-8 pb-24 bg-white shadow-2xl rounded-br-[2rem] sticky left-0 top-[12%]`}
+      className={`bg-white shadow-2xl rounded-br-[2rem] sticky left-0 top-[12%]`}
     >
-      {sidebarLinks.map((link, index) => {
-        const isActive = location.pathname === link.to;
-        const bgColor = isActive ? "#D4EDEC" : "#EAECEB";
-        return (
-          <Link
-            key={index}
-            to={link.to}
-            // onMouseEnter={(e) => {
-            //   if (!link.isActive) {
-            //     e.target.style.backgroundColor = "#D4EDEC";
-            //     e.target.src = link.activeImageSrc;
-            //   }
-            // }}
-            // onMouseLeave={(e) => {
-            //   if (!link.isActive) {
-            //     e.target.style.backgroundColor = "#EAECEB";
-            //     e.target.src = link.inactiveImageSrc;
-            //   }
-            // }}
-          >
-            <div className="flex items-center gap-2 justify-start w-100">
-              <img
-                className={`p-1 bg-[${bgColor}] size-6`}
-                id={link.id}
-                src={isActive ? link.activeImageSrc : link.inactiveImageSrc}
-                alt={`icon${index + 1}`}
-                title={link.title}
-              />
-              {/* {isExpand && (
-                <span
-                  className="text-xs"
-                  style={{
-                    opacity: isExpand ? "1" : "0",
-                    transition: "all ease-out 1s",
-                  }}
-                >
-                  {link.title}
-                </span>
-              )} */}
+      <div
+        className={`flex flex-col ${
+          isSidebarExpanded ? "justify-start px-3" : "items-center"
+        } space-y-12 pt-8 pb-24`}
+        style={{
+          overflowY: "auto",
+        }}
+      >
+        {sidebarLinks.map((link, index) => {
+          const isActive = location.pathname === link.to;
+          const bgColor = isActive ? "#D4EDEC" : "#EAECEB";
 
-              {isSidebarExpanded && (
-                <span
-                  className="text-xs truncate"
-                  style={{
-                    transform: !isSidebarExpanded
-                      ? "translateX(-50px)"
-                      : "translateX(0px)",
-                    opacity: isSidebarExpanded ? "1" : "0",
-                    transition: "all ease-out 0.3s",
-                  }}
-                >
-                  {link.title}
-                </span>
-              )}
+          return (
+            <div
+              key={index}
+              onMouseEnter={() => {
+                if (!showNested.includes(index)) {
+                  setShowNested((prev) => {
+                    return [...prev, index];
+                  });
+                }
+              }}
+              onMouseLeave={() => {
+                if (showNested.includes(index)) {
+                  setShowNested((prev) => {
+                    return prev.filter((idx) => idx !== index);
+                  });
+                }
+              }}
+            >
+              {/* // Render parent link with dropdown menu */}
+              <div>
+                <Link to={link.to} className="hover:text-teal-500">
+                  <div className="flex items-center gap-2 justify-start w-100">
+                    <img
+                      className={`p-1 bg-[${bgColor}] size-6`}
+                      id={link.id}
+                      src={
+                        isActive ? link.activeImageSrc : link.inactiveImageSrc
+                      }
+                      alt={`icon${index + 1}`}
+                      title={link.title}
+                    />
+                    {isSidebarExpanded && (
+                      <span className="text-xs truncate">{link.title}</span>
+                    )}
+                    {link.children && isSidebarExpanded && (
+                      <img src={MenuDropDownArrow} className="w-2 h-100" />
+                    )}
+                  </div>
+                </Link>
+                {link.children && showNested.includes(index) && (
+                  <div className="flex flex-column gap-3 ms-4 mt-2">
+                    {isSidebarExpanded &&
+                      link.children.map((child, childIndex) => (
+                        <Link
+                          key={childIndex}
+                          to={child.to}
+                          className="hover:text-teal-500"
+                        >
+                          <div className="flex items-center gap-2 justify-start w-100 ms-2">
+                            <span className="text-xs truncate">
+                              {child.title}
+                            </span>
+                          </div>
+                        </Link>
+                      ))}
+                  </div>
+                )}
+              </div>
             </div>
-          </Link>
-        );
-      })}
+          );
+        })}
+      </div>
     </div>
   );
 };
