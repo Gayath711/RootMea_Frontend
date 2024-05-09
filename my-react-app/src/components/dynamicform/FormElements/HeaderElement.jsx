d6fe207d4d5e64352a6e50a034ad8c7ae3cbacc9
Raj Kumar D <rajkumar.d@instellars.com>
2024-05-09 08:45:34 +0530

    fix: padding and margin between headers and divider fields

--- my-react-app/src/components/dynamicform/FormElements/HeaderElement.jsx
+++ my-react-app/src/components/dynamicform/FormElements/HeaderElement.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import FormLabel from "./FormLabel";
 
-const HeaderElement = ({ className, type, label, ...rest }) => {
+const HeaderElement = ({ className = "", type, label, ...rest }) => {
   // const Text = React.createElement(type === "header" ? "h1" : "h2", {});
   return (
-    <div className="m-1">
+    <div className={`${type === "header" ? "m-1" : "m-1 mt-0"}`}>
       <h1
         className={`w-full text-gray-700 ${
-          type === "header" ? "font-bold text-base py-2 " : "text-xs py-1"
+          type === "header" ? "font-bold text-base py-1 " : "text-xs py-1"
         } ${className}`}
         {...rest}
       >
